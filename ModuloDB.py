cb7c1d3f9d9fd83de12da9bad28e74bf8e082aff
Paulo Aguiar <pauloaguiars2cg@gmail.com>
2018-12-20 18:41:12 +0000

    Try and Except
    try and except everywhere coz yes

--- ModuloDB.py
+++ ModuloDB.py
@@ -10,22 +10,23 @@ def get_num(x):
 
 
 def convertToValue():
-    
-    calls_df, = pd.read_html("http://www.cee.uma.pt/hardlab/wsngroup/wsn/realtimeinfo.php", header=0,attrs = {'width': '230', 'style': 'font-size:small;color:#333333;'}) #acede tabela com atributos especificados no site especificado
+    try:
+        calls_df, = pd.read_html("http://www.cee.uma.pt/hardlab/wsngroup/wsn/realtimeinfo.php", header=0,attrs = {'width': '230', 'style': 'font-size:small;color:#333333;'}) #acede tabela com atributos especificados no site especificado
 
-    data = calls_df.to_json(orient="records", date_format="iso") #transforma dados em formato json
-    #print(data) #debug
-    #GUARDAR COMO FICHEIRO .json para fazer a API com isso
-    values = json.loads(data) #carrega json como objeto
+        data = calls_df.to_json(orient="records", date_format="iso") #transforma dados em formato json
+        #print(data) #debug
+        #GUARDAR COMO FICHEIRO .json para fazer a API com isso
+        values = json.loads(data) #carrega json como objeto
 
-    valor = []
+        valor = []
 
-    for value in values:
-        #print(get_num(value['Unnamed: 1'])) #imprime valor dentro do unnamed1 usando a função get_enum para retirar unidades
-        valor.append(get_num(value['Unnamed: 1']))        
+        for value in values:
+            #print(get_num(value['Unnamed: 1'])) #imprime valor dentro do unnamed1 usando a função get_enum para retirar unidades
+            valor.append(get_num(value['Unnamed: 1']))        
 
-    return valor
-
+        return valor
+    except:
+        print("falhou")
 #Serve apenas para debug, sendo que todo o codigo sera chamado noutro sitio
 #if __name__ == "__main__":
 
--- Rotina.py
+++ Rotina.py
@@ -11,117 +11,135 @@ from apis import parkingAPI,netNtempAPI,studyRoomSeatsAPI,noiseAPI
 #trata dados
 def handle_data():   
     while(True):
-        insertRep()
-        insertAPI()
-        insertServer()
-        time.sleep(600)
+        try:
+            insertRep()
+            insertAPI()
+            insertServer()
+            time.sleep(600)
+        except:
+            print("failed")
 
 def insertRep():
+    try:
+        valor = convertToValue()
 
-    valor = convertToValue()
+        #trata da inserção dos valores na base de dados nas tabelas respetivas
+        mydb = pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="smartumarep"
+        )
 
-    #trata da inserção dos valores na base de dados nas tabelas respetivas
-    mydb = pymysql.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="smartumarep"
-    )
-
-    insertSQL(mydb, valor)
+        insertSQL(mydb, valor)
+    except:
+        print("failed")
 
 #insert on database
 def insertSQL(mydb, valor):
-    with closing( mydb.cursor() ) as mycursor:
-        mycursor = mydb.cursor()
+    try:
+        with closing( mydb.cursor() ) as mycursor:
+            mycursor = mydb.cursor()
 
-        print(datetime.now()) #debug para o timestamp
-        tim = datetime.now()
-        valor.append('{:%Y-%m-%d %H:%M:%S}'.format(tim))
-        
-        convert_direction(valor)   
+            print(datetime.now()) #debug para o timestamp
+            tim = datetime.now()
+            valor.append('{:%Y-%m-%d %H:%M:%S}'.format(tim))
+            
+            convert_direction(valor)   
 
-        sql = "INSERT INTO weather_weather (temperature, humidity, wind_speed, wind_direction, solar_intensity, timestamp) VALUES ("+str(valor[0])+", "+str(valor[1])+", "+str(valor[3])+", %s, "+str(valor[2])+", %s)"
-        val = (valor[4],valor[5])
-
-        mycursor.execute(sql,val)
-        mydb.commit()
-
-    mydb.close()
-
-#trata da base de dados da API (escrita)
-def insertAPI():
-    valor = convertToValue()
-
-    mydb = pymysql.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="smartumarep"
-    )
-    
-    with closing( mydb.cursor() ) as mycursor:
-        mycursor = mydb.cursor()
-
-        print(datetime.now()) #debug para o timestamp
-        tim = datetime.now()
-        valor.append('{:%Y-%m-%d %H:%M:%S}'.format(tim))
-        
-        convert_direction(valor)   
-
-        #query a base de dados para ver se existe valores na tabela da API
-        mycursor.execute("SELECT * FROM api_weather")
-        myresult = mycursor.fetchall()
-        
-        if len(myresult) > 0:
-            sql = "UPDATE api_weather SET temperature = "+str(valor[0])+" , humidity = "+str(valor[1])+", wind_speed = "+str(valor[3])+", wind_direction = '"+str(valor[4])+"', solar_intensity = "+str(valor[2])+", timestamp = %s WHERE id = 1"
-            val = valor[5]
+            sql = "INSERT INTO weather_weather (temperature, humidity, wind_speed, wind_direction, solar_intensity, timestamp) VALUES ("+str(valor[0])+", "+str(valor[1])+", "+str(valor[3])+", %s, "+str(valor[2])+", %s)"
+            val = (valor[4],valor[5])
 
             mycursor.execute(sql,val)
             mydb.commit()
 
-            send_data(sql)
+        mydb.close()
+    except:
+        print("failed")
 
-        else:
-            sql = "INSERT INTO api_weather (temperature, humidity, wind_speed, wind_direction, solar_intensity, timestamp) VALUES ("+str(valor[0])+", "+str(valor[1])+", "+str(valor[3])+", '"+str(valor[4])+"', "+str(valor[2])+", %s)"
-            val = valor[5]
-            mycursor.execute(sql,val)
-            mydb.commit() 
+#trata da base de dados da API (escrita)
+def insertAPI():
+    try:
+        valor = convertToValue()
 
-            #envia pelo socket
-            send_data(sql)      
+        mydb = pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="smartumarep"
+        )
+        
+        with closing( mydb.cursor() ) as mycursor:
+            mycursor = mydb.cursor()
 
-    mydb.close()
+            print(datetime.now()) #debug para o timestamp
+            tim = datetime.now()
+            valor.append('{:%Y-%m-%d %H:%M:%S}'.format(tim))
+            
+            convert_direction(valor)   
 
+            #query a base de dados para ver se existe valores na tabela da API
+            mycursor.execute("SELECT * FROM api_weather")
+            myresult = mycursor.fetchall()
+            
+            if len(myresult) > 0:
+                sql = "UPDATE api_weather SET temperature = "+str(valor[0])+" , humidity = "+str(valor[1])+", wind_speed = "+str(valor[3])+", wind_direction = '"+str(valor[4])+"', solar_intensity = "+str(valor[2])+", timestamp = %s WHERE id = 1"
+                val = valor[5]
+
+                mycursor.execute(sql,val)
+                mydb.commit()
+
+                send_data(sql)
+
+            else:
+                sql = "INSERT INTO api_weather (temperature, humidity, wind_speed, wind_direction, solar_intensity, timestamp) VALUES ("+str(valor[0])+", "+str(valor[1])+", "+str(valor[3])+", '"+str(valor[4])+"', "+str(valor[2])+", %s)"
+                val = valor[5]
+                mycursor.execute(sql,val)
+                mydb.commit() 
+
+                #envia pelo socket
+                send_data(sql)      
+
+        mydb.close()
+    except:
+        print("failed")
 
 #trata da base de dados principal (escrita)
 def insertServer():
-    valor = convertToValue()
+    try:
+        valor = convertToValue()
 
-    #add time
-    tim = datetime.now()
-    valor.append('{:%Y-%m-%d %H:%M:%S}'.format(tim))
-    
-    #set wind direction
-    convert_direction(valor)
+        #add time
+        tim = datetime.now()
+        valor.append('{:%Y-%m-%d %H:%M:%S}'.format(tim))
+        
+        #set wind direction
+        convert_direction(valor)
 
-    sql = "INSERT INTO weather_weather (temperature, humidity, wind_speed, wind_direction, solar_intensity, timestamp) VALUES ("+str(valor[0])+", "+str(valor[1])+", "+str(valor[3])+", '"+str(valor[4])+"', "+str(valor[2])+", %s)"
-    
-    send_data(sql)
+        sql = "INSERT INTO weather_weather (temperature, humidity, wind_speed, wind_direction, solar_intensity, timestamp) VALUES ("+str(valor[0])+", "+str(valor[1])+", "+str(valor[3])+", '"+str(valor[4])+"', "+str(valor[2])+", %s)"
+        
+        send_data(sql)
+    except:
+        print("failed")
+
 
 def send_data(sql):
-    print(sql)
-    #insert through socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        print(sql)
+        #insert through socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    s.connect(('localhost', 50000))
+        s.connect(('localhost', 50000))
 
-    s.send(str.encode(sql))
+        s.send(str.encode(sql))
 
-    s.close()
-    
+        s.close()
+    except:
+        print("failed")
+
 #set wind direction
 def convert_direction(valor):
+    try:
         if valor[4] >= 0.0 and valor[4] < 45.0:
                 valor[4] = 'N'
                 return
@@ -146,14 +164,18 @@ def convert_direction(valor):
         if valor[4] >= 315.0 and valor[4] < 361.0:
                 valor[4] = 'NW'  
                 return
-
+    except:
+        print("failed")
 
     
 #trata pedidos do servidor 
 def listen_request():
     while (True):
-        insertALLAPIs()
-        time.sleep(600)
+        try:
+            insertALLAPIs()
+            time.sleep(600)
+        except:
+            print("failed")
 
 def insertALLAPIs():
     try:
@@ -242,7 +264,7 @@ def insertALLAPIs():
 
         mydb.close()
     except:
-        print("ta a falhar")
+        print("failed")
 
 #create thread
 thread = Timer(10, handle_data)
