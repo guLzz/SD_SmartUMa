c972d6110a60157e399dc39b0b967fbf6f75c242
Miguel <miguel.alex.andrade@gmail.com>
2019-01-03 15:32:22 +0000

    Final
    Dobby is free!!!

--- smartUMa/smartUMa/averageData/urls.py
+++ smartUMa/smartUMa/averageData/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('parking/', views.parking, name="parking"),
     path('network/', views.network, name="network"),
     path('studyroom/', views.studyroom, name="studyroom"),
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('details/parking/', views.details_parking, name="parking"),
+    path('details/network/', views.details_network, name="network"),
+    path('details/weather/', views.details_weather, name="weather"),
+    path('details/studyroom/', views.details_studyroom, name="studyroom"),
 ]
--- smartUMa/smartUMa/averageData/views.py
+++ smartUMa/smartUMa/averageData/views.py
@@ -14,12 +14,13 @@ def index(request):
     return render(request, 'averageData/index.html', context)
 
 
-def weather(request):
+def get_weather():
     weathers = AverageData.objects.only("weather_id", "timestamp")
     data = []
     for average in weathers:
         value = Weather.objects.get(id=average.weather_id)
         data.insert(0, {
+            "id": value.id,
             "temperature": value.temperature,
             "humidity": value.humidity,
             "wind_speed": value.wind_speed,
@@ -33,15 +34,26 @@ def weather(request):
         'title': 'SmartUMa - Weather'
     }
 
+    return context
+
+
+def weather(request):
+    context = get_weather()
     return render(request, 'averageData/weather.html', context)
 
 
-def parking(request):
+def details_weather(request):
+    context = get_weather()
+    return render(request, 'averageData/details_weather.html', context)
+
+
+def get_parking():
     parks = AverageData.objects.only("parking_id", "timestamp")
     data = []
     for average in parks:
         value = Parking.objects.get(id=average.parking_id)
         data.insert(0, {
+            "id": value.id,
             "occupation": value.occupation,
             "timestamp": average.timestamp
         })
@@ -51,31 +63,50 @@ def parking(request):
         'title': 'SmartUMa - Parking'
     }
 
+    return context
+
+
+def parking(request):
+    context = get_parking()
     return render(request, 'averageData/parking.html', context)
 
 
-def network(request):
+def details_parking(request):
+    context = get_parking()
+    return render(request, 'averageData/details_parking.html', context)
+
+
+def get_network():
     networks = AverageData.objects.only("network_id", "timestamp")
     data = []
     for average in networks:
         value = Network.objects.get(id=average.network_id)
         data.insert(0, {
-                "latency": value.latency,
-                "download": value.download_speed,
-                "upload": value.upload_speed,
-                "timestamp": average.timestamp
-            }
-        )
+            "id": value.id,
+            "latency": value.latency,
+            "download": value.download_speed,
+            "upload": value.upload_speed,
+            "timestamp": average.timestamp
+        }
+                    )
     context = {
         "network": data,
         'title': 'SmartUMa - Network'
     }
-    logger = logging.getLogger(__name__)
-    logger.debug(data)
+    return context
+
+
+def network(request):
+    context = get_network()
     return render(request, 'averageData/network.html', context)
 
 
-def studyroom(request):
+def details_network(request):
+    context = get_network()
+    return render(request, 'averageData/details_network.html', context)
+
+
+def get_studyroom():
     studyrooms = AverageData.objects.only("studyroom_id", "timestamp")
     data = []
     for average in studyrooms:
@@ -86,17 +117,29 @@ def studyroom(request):
         room2pc = Floor2PC.objects.get(id=value.studyroom2PC_id)
         room3 = Floor3.objects.get(id=value.studyroom3_id)
         data.insert(0, {
-                "room0": room0,
-                "room1": room1,
-                "room2": room2,
-                "room2PC": room2pc,
-                "room3": room3,
-                "timestamp": average.timestamp
-            }
-        )
+            "id": value.id,
+            "room0": room0,
+            "room1": room1,
+            "room2": room2,
+            "room2PC": room2pc,
+            "room3": room3,
+            "timestamp": average.timestamp
+        }
+                    )
 
     context = {
         "studyroom": data,
         'title': 'SmartUMa - Study Rooms'
     }
+
+    return context
+
+
+def studyroom(request):
+    context = get_studyroom()
     return render(request, 'averageData/studyroom.html', context)
+
+
+def details_studyroom(request):
+    context = get_studyroom()
+    return render(request, 'averageData/details_studyroom.html', context)
